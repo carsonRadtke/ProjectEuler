13aceac9701b68c2f9c2fd7f90ab7a2f3a14646c
carson <cradtke@caraflow.com>
2020-06-05 17:39:38 -0500

    made 10 a little faster. still not happy

--- python/problem010.py
+++ python/problem010.py
@@ -1,18 +1,20 @@
-from math import sqrt
+from math import sqrt, ceil
 
 def isPrime(n):
-    if (n == 2):
-        return True
-    for x in range(2, int(sqrt(n))+1):
+    sq = sqrt(n)
+    bound = ceil(sq)+1
+    for x in range(2, bound):
         if (n % x == 0):
             return False
     return True
 
 def main():
-    output = 0
-    for x in range(2, 2000000):
+    output = 2 + 3
+    for x in range(5, 2000000, 6):
         if (isPrime(x)):
             output = output + x
+        if (isPrime(x+2)):
+            output = output + x + 2
     print(output)
 
 main()
