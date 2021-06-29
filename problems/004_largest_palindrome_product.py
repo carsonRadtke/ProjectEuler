8ad5f8e560efeed34a17793fd298a6717bd9dc6a
carson radtke <nosrac925@gmail.com>
2021-06-29 10:40:14 -0500

    problem 4 was in the wrong place

--- problems/004_largest_palindrome_product.py
+++ problems/004_largest_palindrome_product.py
@@ -2,7 +2,7 @@ def is_palindrome(s):
     return str(s) == str(s)[::-1]
 
 def main():
-    products_of_3dns = [str(x*y) for x in range(y, 100) for y in range(10, 100)]
+    products_of_3dns = [x*y for x in range(100, 1000) for y in range(100, 1000)]
     palindromes_of_3dns = filter(is_palindrome, products_of_3dns)
     largest_palindrome_product = max(palindromes_of_3dns)
 
